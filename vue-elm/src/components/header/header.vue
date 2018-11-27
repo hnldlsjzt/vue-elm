a0a88ef4968c383b39d1f19ad5fb196ecd54b4eb
小二上酒 <819163569@qq.com>
2018-11-27 10:13:13 +0800

    增加模拟数据

---
+++ vue-elm/src/components/header/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+      我是header
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
